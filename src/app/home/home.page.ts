cf6339e661f1e9fcbdbba7d0780e77e8400cefa3
jair <ajair.sanchez@sigsa.com.mx>
2024-05-28 12:08:12 -0600

    implementacion de arcgis map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+
+
+  constructor() {
+  }
+
+  private latitude: number = 0;
+  private longitude: number = 0;
+
+
+  public async ngOnInit()  {
+    const position = await Geolocation.getCurrentPosition();
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+
+    const map = new Map({
+      basemap: "topo-vector"
+    });
+
+    const view = new MapView({
+      container: "container",
+      map: map,
+      zoom: 4,
+      center: [this.longitude, this.latitude],
+    })
+  }
+
+
 
 }
