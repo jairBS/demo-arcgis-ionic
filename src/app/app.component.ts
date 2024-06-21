a41a0be776ddb2375ce27d1ffa163a7db2a0b6e4
jair <ajair.sanchez@sigsa.com.mx>
2024-06-21 08:56:35 -0600

    cerrar app con boton atras android en app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// para boton atras de android
+import { Optional } from '@angular/core';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  // TODO: Validar si no hay un proceso pendiente antes de cerrar la app.
+  // para boton atras de android
+  constructor(private platform: Platform, @Optional() private routerOutlet?: IonRouterOutlet) {
+    // para boton atras de android
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if(!this.routerOutlet?.canGoBack()) {
+        App.exitApp();
+      }
+    })
+  }
 }
